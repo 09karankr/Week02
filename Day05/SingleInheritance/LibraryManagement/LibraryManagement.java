625cd54b5a5f0d93e2b8bc098142be7c205d3c1a
Karan Kumar <officialkaran39@gmail.com>
2025-01-24 20:18:02 +0530

    Library management program completed with the use of single inheritance

---
+++ Day05/SingleInheritance/LibraryManagement/LibraryManagement.java
@@ -0,0 +1,8 @@
+package LibraryManagement;
+
+public class LibraryManagement {
+    public static void main(String[] args){
+       Author author = new Author("Effective Java",2008, "Rakesh Pd","Rakesh Pd is a software engineer and author");
+       author.displayInfo();
+    }
+}
