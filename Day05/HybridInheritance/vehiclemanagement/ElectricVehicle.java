8461a440547a7f3e8bfcadc27aa05192f252084b
Karan Kumar <officialkaran39@gmail.com>
2025-01-25 15:37:40 +0530

    Vehicle Management system completed

---
+++ Day05/HybridInheritance/vehiclemanagement/ElectricVehicle.java
@@ -0,0 +1,19 @@
+package vehiclemanagement;
+
+public class ElectricVehicle extends  Vehicle{
+    private int batteryCapacity;
+    public ElectricVehicle(String model, int maxSpeed, int batteryCapacity) {
+        super(model, maxSpeed);
+        this.batteryCapacity = batteryCapacity;
+    }
+    public void charge() {
+        System.out.println("Charging the electric vehicle... Battery capacity: " + batteryCapacity + " kWh");
+    }
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Battery Capacity: " + batteryCapacity + " kWh");
+    }
+
+
+}
